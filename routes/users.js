df250642f92b4e87327e982ac7b0d20942072cf5
BelascuDorin <Dorin.Belascu@gmail.com>
2020-12-21 16:56:53 +0200

    First time to add a user to MongoDB online cloud.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -16,13 +17,42 @@ router.post('/', [
         'password', 
         'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
-], (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() });
-    }
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    res.send('passed');
-});
+        const { name, email, password} = req.body;
+
+        try{
+            let user = await User.findOne({ email });
+
+            if(user){
+                return res.status(400).json({ msg: 'User already exists' });
+            }
+
+            user = new User({
+                name, email, password // the same as name: name, email: email...
+            });
+
+            //encript password
+            const salt = await bcrypt.genSalt(10); // how secure the salt is
+            user.password = await bcrypt.hash(password, salt);
+            try{
+                await user.save();
+            }
+            catch{
+                console.log(err);
+            }
+
+            res.send('User saved');
+
+        } catch(err){
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports = router;
